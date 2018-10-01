eb8d2477dcd40b540f3f26fadf81e9a80a05ecab
Nabil <nabil@Admins-MacBook-Pro.local>
2018-10-01 15:08:07 -0400

    Added pull to refresh

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -15,43 +15,53 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     
     var movies: [[String : Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    tableView.dataSource = self
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        tableView.dataSource = self
+        tableView.rowHeight = 200
+        fetchMovies()
+        
+    }
     
-    tableView.rowHeight = 200
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a5e1894a916adacf4ef0c0e2758f8dcd")!
         
-    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a5e1894a916adacf4ef0c0e2758f8dcd")!
-        
-    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         
         // normally wouldn't want to use reloadIgnoringLocalCacheData, only used here for testing.
         
-    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-    
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
         let task = session.dataTask(with: request) { (data, response, error) in
             // the following code runs once the network request returns
             if let error = error {
-                    print(error.localizedDescription)
+                print(error.localizedDescription)
             }
             else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String : Any]]
-        
+                
                 self.movies = movies
                 
                 //for movie in movies{
-                   // let title = movie["title"] as! String
-                    //print(title)
+                // let title = movie["title"] as! String
+                //print(title)
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
                 
             }
             
         }
         task.resume()
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
